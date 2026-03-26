af59a142440b80e26e391c35a6741991aafd9851
ashmitxnarkhede <ashmitxnarkhede10@gmail.com>
2026-03-26 20:44:15 +0530

    Used Brute method

--- Array/Twosums.py
+++ Array/Twosums.py
@@ -30,15 +30,15 @@ Only one valid answer exists.
 """
 
 #Method 1
-"""class Solution:
+class Solution:
     def twoSum(self,nums,target):
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
         return []
-"""   
-#Method 2
+   
+
 
 
 

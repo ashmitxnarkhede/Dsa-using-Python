0fb3330718c1877e025733bd9b1bc44bf1d6294d
ashmitxnarkhede <ashmitxnarkhede10@gmail.com>
2026-03-29 12:36:16 +0530

    just for practise

---
+++ Array/practise.py
@@ -0,0 +1,46 @@
+"""Write Python code to:
+1. Print the first and last number
+2. Add the number 99 to the end
+3. Change the number 3 to 300
+4. Print the total count of items in the list"""
+"""
+#1. Print the first and last number
+numbers = [1, 2, 3, 4, 5]
+print("First Number & Last Number:", numbers[0], numbers[-1])
+#2. Add the number 99 to the end
+numbers.append(99)
+print("After adding 99 to the end:", numbers)
+#3. Change the number 3 to 300
+i=numbers.index(3)
+numbers[i] = 300
+print("After changing 3 to 300:", numbers)
+#4. Print the total count of items in the list
+print("Total count of items in the list:", len(numbers))
+"""
+
+#Two Pointers Approach
+#sorted array
+nums=[1,2,3,4,5,6]
+target=8
+"""
+for i in range(len(nums)):
+    for j in range(i+1, len(nums)):
+        if nums[i]+nums[j] == target:
+            print(nums[i], nums[j])
+"""
+
+#Optimizing for two pointer technique
+
+left=0
+right=len(nums)-1
+
+while left<right:
+    sum=nums[left]+nums[right]
+    if sum==target:
+        print(nums[left], nums[right])
+        left+=1
+        right-=1
+    elif sum<target:
+        left+=1
+    else:
+        right-=1
